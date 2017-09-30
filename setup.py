020d2127c31c74786aab6f2e2abb3d2415cc03b3
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-09-30 12:06:58 +0200

    add name and version to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
+    name='tinyexpr',
+    version='0.1.0',
     ext_modules=cythonize([Extension('tinyexpr',
                                      ['tinyexpr.pyx'],
                                      extra_objects=['include/tinyexpr.c']
