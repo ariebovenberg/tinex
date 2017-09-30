24d6574b75967228a7636cb8e22c4d60adc47e72
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-09-30 10:25:27 +0200

    add test for non-numbers

--- test_tinyexpr.py
+++ test_tinyexpr.py
@@ -1,4 +1,5 @@
 import pytest
+import math
 
 import tinyexpr as te
 
@@ -9,5 +10,9 @@ class TestEval:
         assert te.eval(b'1+1') == 2
 
     def test_parse_error(self):
-        with pytest.raises(SyntaxError, match='4'):
+        with pytest.raises(SyntaxError, match='position 4'):
             te.eval(b'(5+5')
+
+    def test_non_numbers(self):
+        assert te.eval(b'1/0') == float('inf')
+        assert math.isnan(te.eval(b'0/0'))
